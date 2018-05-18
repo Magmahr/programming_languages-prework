cda6ad7f82f53625efd519c33a87a41e354f4584
Matt Maher <mattmaher0@gmail.com>
2018-05-18 15:39:26 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -33,10 +33,12 @@ languages = {
 def reformat_languages(languages)
   new_hash = {}
     languages[:oo].each do |language|
-      new_hash << language[:style] = [:oo]
+      language[:style] = []
+      language[:style] << :oo
     end
     languages[:functional].each do |language|
-      new_hash << language[:style] = [:functional]
+      language[:style] = []
+      language[:style] << :functional
     end  
   new_hash   
 end
