9ff7ab7d10f03e734e46dcb8dea5fff9c199b05e
Matt Maher <mattmaher0@gmail.com>
2018-05-18 15:35:41 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,4 +1,42 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
 def reformat_languages(languages)
   new_hash = {}
-  
+    languages[:oo].each do |language|
+      new_hash << language[:style] = :oo
+    end
+    languages[:functional].each do |language|
+      new_hash << language[:style] = :functional
+    end  
+  new_hash   
 end
